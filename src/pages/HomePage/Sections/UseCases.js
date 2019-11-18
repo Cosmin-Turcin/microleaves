481b8e1184c0ba8a1befdd12a09397b12ba2ee94
Cosmin Turcin <cosmin.turcin@udevoffice.ro>
2019-11-18 10:09:34 +0200

    feat: create component for use cases homepage section

---
+++ src/pages/HomePage/Sections/UseCases.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import case1 from '../../../assets/images/cases-1.svg';
+import case2 from '../../../assets/images/cases-2.svg';
+import case3 from '../../../assets/images/cases-3.svg';
+import case4 from '../../../assets/images/cases-4.svg';
+import case5 from '../../../assets/images/cases-5.svg';
+import case6 from '../../../assets/images/cases-6.svg';
+import case7 from '../../../assets/images/cases-7.svg';
+import case8 from '../../../assets/images/cases-8.svg';
+
+const UseCases = () => {
+  return (
+    <div className="UseCases">
+      <div className="container">
+        <h2>Perfect For All Use Cases</h2>
+        <div className="row UseCases__cases">
+          <div className="col col-3 text-center">
+            <img src={case1} alt="" />
+            <h3>Price Comparation</h3>
+          </div>
+          <div className="col col-3 text-center">
+            <img src={case2} alt="" />
+            <h3>Ad Verification</h3>
+          </div>
+          <div className="col col-3 text-center">
+            <img src={case3} alt="" />
+            <h3>Sales Intelligence</h3>
+          </div>
+          <div className="col col-3 text-center">
+            <img src={case4} alt="" />
+            <h3>Brand Protection</h3>
+          </div>
+          <div className="col col-3 text-center">
+            <img src={case5} alt="" />
+            <h3>Web Property Testing</h3>
+          </div>
+          <div className="col col-3 text-center">
+            <img src={case6} alt="" />
+            <h3>SEO</h3>
+          </div>
+          <div className="col col-3 text-center">
+            <img src={case7} alt="" />
+            <h3>Online Shopping: Sneakers & Tickets</h3>
+          </div>
+          <div className="col col-3 text-center">
+            <img src={case8} alt="" />
+            <h3>Web Scaping</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UseCases
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LargestProxy from './Sections/LargestProxy';
 import Designed from './Sections/Designed';
+import UseCases from './Sections/UseCases';
 
 const HomePage = () => {
   return (
     <div className="HomePage">
       <LargestProxy />
       <Designed />
+      <UseCases />
     </div>
   )
 }
